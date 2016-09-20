033a2eadaa50eacef2017f3214d8d8b039f8da14
Michael D. Mooring <mikeumus@gmail.com>
2016-09-20 01:15:24 +0000

    angular-cli migration.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,2 @@
-// export * from './environment';
-export * from './ng2comment.component';
+export {environment} from './environment';
+export {Ng2commentAppComponent} from './ng2comment.component';
